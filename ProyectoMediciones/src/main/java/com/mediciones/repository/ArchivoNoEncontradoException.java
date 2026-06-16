b23ab8e445f5ce03aef0e991e2fbd5ff68f1e5b5
salvarg8 <salvarodriguez_91@hotmail.com>
2026-06-16 12:02:15 -0300

    corrijo extend de la exception ArchivoNoEncontradoException

--- ProyectoMediciones/src/main/java/com/mediciones/repository/ArchivoNoEncontradoException.java
+++ ProyectoMediciones/src/main/java/com/mediciones/repository/ArchivoNoEncontradoException.java
@@ -1,6 +1,12 @@
 package com.mediciones.repository;
 
-public class ArchivoNoEncontradoException extends Throwable {
-    public ArchivoNoEncontradoException(String s) {
+public class ArchivoNoEncontradoException extends Exception {
+
+    public ArchivoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+
+    public ArchivoNoEncontradoException(String mensaje, Throwable causa) {
+        super(mensaje, causa);
     }
 }
